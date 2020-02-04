c5ab4a6e4423de708ee63cc88ffa90fc9ebb98d7
mih <mihmin98@yahoo.com>
2020-02-04 18:51:01 +0200

    Added SHA256

---
+++ sha256.hpp
@@ -0,0 +1,12 @@
+#ifndef _SHA256_HPP_
+#define _SHA256_HPP_
+
+#ifndef __cplusplus
+#error Do not include the hpp header in a c project!
+#endif //__cplusplus
+
+extern "C" {
+#include "sha256.h"
+}
+
+#endif //_SHA256_HPP_
