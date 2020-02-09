fbbb7d8884c6e858b805e6dc016345a85548438e
mih <mihmin98@yahoo.com>
2020-02-09 15:39:07 +0200

    Implemented AddMenu class

---
+++ AddMenu.cpp
@@ -0,0 +1,77 @@
+#include "AddMenu.hpp"
+
+AddMenu::AddMenu(MenuContext *context)
+{
+    this->Run(context);
+}
+
+void AddMenu::Run(MenuContext *context)
+{
+    LoginInfo *login = new LoginInfo;
+    std::string input = "";
+
+    // get platform
+    std::cout << "\t\tPassMan\n\tAdd new entry\n\nEnter Platfrom name: ";
+    std::getline(std::cin, input);
+
+    while (input == "")
+    {
+        std::cout << "Input cannot be empty\n> ";
+        std::getline(std::cin, input);
+    }
+
+    login->SetPlatform(input);
+
+    // get name
+    input = "";
+    std::cout << "\nEnter Username: ";
+    std::getline(std::cin, input);
+
+    while (input == "")
+    {
+        std::cout << "Input cannot be empty\n> ";
+        std::getline(std::cin, input);
+    }
+
+    login->SetUsername(input);
+
+    // get password
+    input = "";
+    std::cout << "\nEnter Password: ";
+    std::getline(std::cin, input);
+
+    while (input == "")
+    {
+        std::cout << "Input cannot be empty\n> ";
+        std::getline(std::cin, input);
+    }
+
+    login->SetPassword(input);
+
+    // get extra info
+    input = "";
+    std::cout << "\nEnter Username: ";
+    std::getline(std::cin, input);
+
+    login->SetExtraInfo(input);
+
+    // confirm to add
+    std::cout << "\n"
+              << login->GetString() << "\n\nAre you sure you want to add this entry? (Y/n): ";
+    std::getline(std::cin, input);
+
+    // if yes then update session info
+    if (input[0] == 'y' || input[0] == 'Y')
+    {
+        SessionInfo *session = SessionInfo::GetInstance();
+        session->AddLoginInfoVector(login);
+        session->SetUnsavedChanges(true);
+
+        std::cout << "Entry added successfully\n";
+        std::cin.get();
+    }
+
+    // set state to main menu
+    context->SetState(new MainMenu(context));
+    delete this;
+}
---
+++ AddMenu.hpp
@@ -0,0 +1,20 @@
+#ifndef _ADD_MENU_HPP_
+#define _ADD_MENU_HPP_
+
+#pragma once
+#include "Menu.hpp"
+#include "MenuContext.hpp"
+#include "MainMenu.hpp"
+#include "SessionInfo.hpp"
+#include "LoginInfo.hpp"
+#include <iostream>
+
+class AddMenu : public Menu
+{
+public:
+    AddMenu(MenuContext *context);
+    void Run(MenuContext *context) override;
+
+private:
+};
+#endif
